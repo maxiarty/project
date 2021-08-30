db3664beeb3f6228362b76196d26a33b7d7c16da
Maxim <maxjke2h@gmail.com>
2021-08-30 15:45:18 +0300

    первое задание

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 "use strict";
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '0');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+};
+
+let filmOne = prompt('Один из последних просмотреных фильмов?', '0'),
+    filmOneRating = +prompt('Ваша оценка этого фильма?', '0'),
+    filmTwo = prompt('Один из последних просмотреных фильмов?', '0'),
+    filmTwoRating = +prompt('Ваша оценка этого фильма?', '0');
+
+personalMovieDB.movies[filmOne] = filmOneRating;
+
+personalMovieDB.movies[filmTwo] = filmTwoRating;
+
+console.log(personalMovieDB);
